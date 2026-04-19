9edc358c4b06c0e0821996b459368ca50eb07943
Eshwar <84226229+eshwardudala@users.noreply.github.com>
2026-04-19 13:47:41 -0400

    Refactor Snowflake connection to use variables
    Replaced hardcoded Snowflake credentials with variables.

--- analytics.py
+++ analytics.py
@@ -5,13 +5,13 @@ from datetime import datetime
 # Snowflake connection
 def get_snowflake_connection():
     conn = snowflake.connector.connect(
-        user='ESHWARDUDALA0',
-        password='Telangana@5617',
-        account='HZCTIQT-PRC84963',
-        warehouse='COMPUTE_WH',
-        database='FINANCE_AI',
-        schema='CORE'
-    )
+    user= USER,
+    password=PASSWORD,
+    account= Account,
+    warehouse='COMPUTE_WH',
+    database='FINANCE_AI',
+    schema='CORE'
+)
     return conn
 
 # Analytics queries with Cortex functions
