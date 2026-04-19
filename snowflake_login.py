768c661dc3f9ad723dfa5928eb67107f8efc386c
Eshwar <84226229+eshwardudala@users.noreply.github.com>
2026-04-19 14:11:02 -0400

    Delete snowflake_login.py

--- snowflake_login.py
+++
@@ -1,9 +0,0 @@
-# Snowflake connection
-conn = snowflake.connector.connect(
-    user='ESHWARDUDALA0',
-    password='Telangana@5617',
-    account='HZCTIQT-PRC84963',
-    warehouse='COMPUTE_WH',
-    database='FINANCE_AI',
-    schema='CORE'
-)
