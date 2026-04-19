7104270d70dddb93bbd7e26caed99a39c6443498
Eshwar <84226229+eshwardudala@users.noreply.github.com>
2026-04-19 13:47:59 -0400

    Refactor Snowflake connection to use variables
    Replaced hardcoded Snowflake connection credentials with variables.

--- chatbot.py
+++ chatbot.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 # Snowflake connection
 def get_snowflake_connection():
-    conn = snowflake.connector.connect(
-        user='ESHWARDUDALA0',
-        password='Telangana@5617',
-        account='HZCTIQT-PRC84963',
-        warehouse='COMPUTE_WH',
-        database='FINANCE_AI',
-        schema='CORE'
-    )
+   conn = snowflake.connector.connect(
+    user= USER,
+    password=PASSWORD,
+    account= Account,
+    warehouse='COMPUTE_WH',
+    database='FINANCE_AI',
+    schema='CORE'
+)
     return conn
 
 
