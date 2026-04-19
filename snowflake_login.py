d25e31dbcd80a6e5ab801ccd16828b82f04e81e4
Eshwar <84226229+eshwardudala@users.noreply.github.com>
2026-04-19 13:39:30 -0400

    Establish Snowflake connection in snowflake_login.py
    Added Snowflake connection configuration.

--- snowflake_login.py
+++ snowflake_login.py
@@ -1,1 +1,9 @@
-
+# Snowflake connection
+conn = snowflake.connector.connect(
+    user='ESHWARDUDALA0',
+    password='Telangana@5617',
+    account='HZCTIQT-PRC84963',
+    warehouse='COMPUTE_WH',
+    database='FINANCE_AI',
+    schema='CORE'
+)
